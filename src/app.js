06c2c99bd0c1c18f66fae0489de4c251a9a1b0c2
张一帆 <zhangyifan@MacBook-Pro-de-zhangyifan.local>
2025-07-07 23:41:02 +0800

    feat:CA证书

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 import express from 'express';
 import userRoutes from './routes/userRoutes.js';
 import recordRoutes from './routes/recordRoutes.js';
